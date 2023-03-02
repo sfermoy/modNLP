642dd211919d3ceaa74c9d5e01fe6c17bc6ad627
Nino Luz <luzs@acm.org>
2023-03-02 17:54:35 +0000

    Tidied up ExpRangeSlider code. Added granularity parameter.

--- src/modnlp/gui/ExpRangeSlider.java
+++ src/modnlp/gui/ExpRangeSlider.java
@@ -2,6 +2,19 @@ package modnlp.gui;
 
 import java.lang.Math;
 import java.text.DecimalFormat;
+import java.awt.Insets;
+import java.awt.GridBagConstraints;
+import java.awt.Dimension;
+import javax.swing.JLabel;
+import javax.swing.JFrame;
+import java.awt.GridBagLayout;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ChangeEvent;
 
 /**
  * An extension of RangeJSlider to select a range of values on an
@@ -10,31 +23,39 @@ import java.text.DecimalFormat;
  */
 public class ExpRangeSlider extends RangeSlider {
 
-  private int offset = 50;
-  private double div = 50;
+  // The in slide value will be scaled according to an exp of this base 
   private int base = 10;
+  // the minimum value of the slider (after scaling; ie not the value in the super class)
   private int min = 0;
-  private int max = 150;
+  // the maximum value
+  private int max = 100;
+
+  // granularity controls the spacing of the decimal steps
+  private double granularity = 50;
+
+  // the default value for the zero (will be converted to negative)
+  private int zero = 40;
+  
   private DecimalFormat df = new DecimalFormat("#.##");
   
   public ExpRangeSlider() {
     super();
   }
-
-  public void setOffset (int v){
-    offset = v;
-  }
-
-  public int getOffset (){
-    return offset;
-  }
-
+  
   public void setBase (int v){
     base = v;
   }
 
   public int getBase (){
     return base;
+  }
+
+  public void setZero (int v){
+    zero = v;
+  }
+
+  public int getZero (){
+    return zero;
   }
 
   
@@ -54,21 +75,58 @@ public class ExpRangeSlider extends RangeSlider {
     return max;
   }
 
-  public void setDiv (double v){
-    div = v;
+  public void setGranularity (double v){
+    granularity = v;
+    setMaximum(super.getMaximum());
+    setMinimum(super.getMinimum());
+    //System.err.println("Max: "+max+" Min:"+min+" MaxU"+super.getMaximum()+" MinU"+super.getMinimum());
+
   }
 
-  public double getDiv (){
-    return div;
+  public double getGranularity (){
+    return granularity;
   }
 
   public void setMaximum(int v){
-    max = v+offset;
+    max = (int)Math.round(granularity *
+                          ExpRangeSlider.log_n((double)base, (double)v));
     super.setMaximum(max);
   }
+
+  public void setMinimum(int v){
+    setMinimum((double) v);
+  }
+    
+  public void setMinimum(double v){
+    if (v <= 0)
+      min = (int)Math.round(-1 * granularity *
+                            ExpRangeSlider.log_n((double)base, zero));
+    else
+      min = (int) Math.round(granularity*
+                             ExpRangeSlider.log_n(base, v));
+    super.setMinimum(min);
+  }
   
+  private static final double log_n(double base, double v){
+    return(Math.log(v)/Math.log(base));
+  }
+
+  public void setLowerValue(double v){
+    //System.err.println("SVd: "+v);
+    super.setValue(getUnscaledValue(v));
+  }
+
+  //public void setValue(int v){
+  //  System.err.println("SVi: "+v);
+  //  super.setValue(getUnscaledValue(v));
+  //}
+
+  public void setUpperValue(double v){
+    super.setUpperValue(getUnscaledValue(v));
+  }
+
   public void setUpperValue(int v){
-    super.setUpperValue(v+offset);
+    super.setUpperValue(getUnscaledValue(v));
   }
   
   public String getExpValueString (double v){
@@ -78,21 +136,115 @@ public class ExpRangeSlider extends RangeSlider {
   public double getExpValue () {
     int v = super.getValue();
     double e = getScaledExp(v);
-    //System.err.println("V: "+v+" E:"+e);
     return e;
   }
 
   public double getExpUpperValue () {
     int v = super.getUpperValue();
     double e = getScaledExp(v);
-    //System.err.println("UV: "+v+" UE:"+e);
     return e;
   }
 
   private double getScaledExp(int v){
-    return Math.pow(base, (v-offset)/div);
+    //return Math.pow(base, (v-offset)/div);
+    return Math.pow(base, v/granularity);
   }
+
+  private int getUnscaledValue(double v){
+    //return Math.pow(base, (v-offset)/div);
+    int e;
+    if ( v == 0.0)
+      e = min;
+    else
+      e = (int) Math.round(log_n(base, v)*granularity);
+    return e;
+  }
+
   
+
+  /**
+   * Test slider
+   * @param args String[]
+   */
+  public static void main(String[] args) {
+    ExpRangeSlider expsl = new ExpRangeSlider();
+
+    try {
+      UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+
+    SwingUtilities.invokeLater(new Runnable() {
+        public void run() {
+          JPanel expslPanel = new JPanel(new GridBagLayout());
+          JLabel expslValue1 = new JLabel();
+          JLabel expslValue2 = new JLabel();
+             // range slider settings
+          expslValue1.setHorizontalAlignment(JLabel.LEFT);
+          expslValue2.setHorizontalAlignment(JLabel.LEFT);        
+          expsl.setPreferredSize(new Dimension(220,
+                                               expsl.getPreferredSize().height));
+          expsl.setMinimum(0);
+          expsl.setMaximum(100);
+          expsl.setGranularity(50);
+          expsl.setZero(50);
+          
+          expsl.setLowerValue(0);
+          expsl.setUpperValue(100);
+        
+          // Initialize value display.
+          expslValue1.setText("0");
+          expslValue2.setText(expsl.getExpValueString(expsl.getExpUpperValue()));
+          final JLabel expslTitle = new JLabel("Frequency range", JLabel.CENTER);
+          
+          expslPanel.add(expslValue1,
+                         new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
+                                                GridBagConstraints.NORTHWEST,
+                                                GridBagConstraints.NONE,
+                                                new Insets(0, 0, 3, 0), 0, 0));
+          expslPanel.add(expslTitle,
+                         new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
+                                                GridBagConstraints.NORTH,
+                                                GridBagConstraints.NONE,
+                                                new Insets(0, 0, 6, 0), 0, 0));
+          expslPanel.add(expslValue2,
+                         new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
+                                                GridBagConstraints.NORTHEAST,
+                                                GridBagConstraints.NONE,
+                                                new Insets(0, 0, 6, 0), 0, 0));
+          expslPanel.add(expsl      ,
+                         new GridBagConstraints(0, 2, 2, 1, 0.0, 0.0,
+                                                GridBagConstraints.NORTHWEST,
+                                                GridBagConstraints.NONE,
+                                                new Insets(0, 0, 0, 0), 0, 0));
+
+          expsl.addChangeListener(new ChangeListener() {
+              public void stateChanged(ChangeEvent e) {
+                ExpRangeSlider slider = (ExpRangeSlider) e.getSource();
+                double minFreq = slider.getExpValue();
+                double maxFreq = slider.getExpUpperValue();
+                expslValue1.setText(slider.getExpValueString(minFreq));
+                expslValue2.setText(slider.getExpValueString(maxFreq));
+              }
+            });
+          // Create window frame.
+          JFrame frame = new JFrame();
+          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+          frame.setResizable(false);
+          frame.setTitle("ExpRange Slider Demo");
+        
+          // Set window content and validate.
+          frame.getContentPane().setLayout(new BorderLayout());
+          frame.getContentPane().add(expslPanel, BorderLayout.CENTER);
+          frame.pack();
+        
+          // Set window location and display.
+          frame.setLocationRelativeTo(null);
+          frame.setVisible(true);
+        }
+      });
+  }
   
   
 }
--- src/modnlp/prj.el
+++ src/modnlp/prj.el
@@ -1,5 +1,51 @@
 (jdee-project-file-version "1.0")
 (jdee-set-variables
- '(jdee-compile-option-encoding "UTF-8"))
-;;; JDE Project file
-;;; PRJ.EL ends heres
+ '(jdee-project-name "modnlp-gui")
+ '(jdee-db-option-application-args nil)
+ '(jdee-db-read-app-args t)
+ '(jdee-global-classpath (quote ("/home/luzs/lib/java/Prefuse/build/prefuse/classes" ".." "../../lib/gnu-regexp.jar" "../../lib/je.jar" "../../lib/antlr-2.7.6.jar" "../../lib/commons-pool-1.2.jar" "../../lib/exist-modules.jar" "../../lib/exist.jar" "../../lib/jgroups-all.jar" "../../lib/log4j-1.2.14.jar" "../../lib/resolver.jar" "../../lib/sunxacml.jar" "../../lib/xmldb.jar" "../../lib/MinML2.jar" "../../lib/xmlrpc-1.2-patched.jar" "../../lib/prefuse.jar" "./../../lib/lucene-core-3.6.0.jar" "./../../lib/lucene-analyzers-3.6.0.jar" "./../../lib/lucene-kuromoji-3.6.0.jar"  
+;"/tmp/libs/wordleconc.jar"
+"./../../teccli/plugins/WordleConc/lib/wordleconc.jar"
+;"./../../teccli/org-modnlp/Mosaic/lib/mosaic.jar"
+;"../../teccli/org-modnlp/ConcTree/lib/conctree.jar"
+"./../../teccli/org-modnlp/ConcTree/classes"
+"./../../teccli/org-modnlp/Metafacet/classes"
+"./../../teccli/org-modnlp/ComFre/classes"
+"./../../teccli/org-modnlp/ComFre/lib/commons-io.jar"
+"./../../teccli/org-modnlp/Mosaic/classes"
+"./../../lib/stanford-segmenter-3.8.0.jar"
+"/usr/share/java/javafx-base.jar"
+"/usr/share/java/javafx-controls.jar"
+"/usr/share/java/javafx-fxml.jar"
+"/usr/share/java/javafx-graphics.jar"
+"/usr/share/java/javafx-media.jar"
+"/usr/share/java/javafx-swing.jar"
+"/usr/share/java/javafx-web.jar"
+)))
+ '(jdee-run-working-directory "../../")
+ '(jdee-run-option-application-args nil)
+ '(jdee-run-read-app-args t)
+ '(jdee-sourcepath (quote ("./../../teccli/org-modnlp/ConcTree/src" "./../../teccli/org-modnlp/Mosaic/src" "./../.." "/home/luzs/lib/java/Prefuse/build/prefuse/classes")))
+ '(jdee-run-application-class "")
+ '(jdee-compile-option-debug (quote ("all" (t nil nil)))))
+
+;; ;;; JDE compat
+;; (jde-project-file-version "1.0")
+;; (jde-set-variables
+;;  '(jde-project-name "modnlp-idx")
+;;  '(jde-db-option-application-args nil)
+;;  '(jde-db-read-app-args t)
+;;  '(jde-global-classpath (quote ("/home/luzs/lib/java/Prefuse/build/prefuse/classes" "./../.." "../../lib/gnu-regexp.jar" "../../lib/je.jar" "../../lib/antlr-2.7.6.jar" "../../lib/commons-pool-1.2.jar" "../../lib/exist-modules.jar" "../../lib/exist.jar" "../../lib/jgroups-all.jar" "../../lib/log4j-1.2.14.jar" "../../lib/resolver.jar" "../../lib/sunxacml.jar" "../../lib/xmldb.jar" "../../lib/MinML2.jar" "../../lib/xmlrpc-1.2-patched.jar" "../../lib/prefuse.jar" "./../../lib/lucene-core-3.6.0.jar" "./../../lib/lucene-analyzers-3.6.0.jar" "./../../lib/lucene-kuromoji-3.6.0.jar"  
+;; ;"/tmp/libs/wordleconc.jar"
+;; "./../../teccli/plugins/WordleConc/lib/wordleconc.jar"
+;; ;"./../../teccli/org-modnlp/Mosaic/lib/mosaic.jar"
+;; ;"../../teccli/org-modnlp/ConcTree/lib/conctree.jar"
+;; "./../../teccli/org-modnlp/ConcTree/classes"
+;; "./../../teccli/org-modnlp/Mosaic/classes"
+;; )))
+;;  '(jde-run-working-directory "../../")
+;;  '(jde-run-option-application-args nil)
+;;  '(jde-run-read-app-args t)
+;;  '(jde-sourcepath (quote ("./../../teccli/org-modnlp/ConcTree/src" "./../../teccli/org-modnlp/Mosaic/src" "./../.." "/home/luzs/lib/java/Prefuse/build/prefuse/classes")))
+;;  '(jde-run-application-class "")
+;;  '(jde-compile-option-debug (quote ("all" (t nil nil)))))
